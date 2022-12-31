137c4a04ddcc91c19f80c9a12922203200df8544
Sreelal TS <heysreelal@gmail.com>
2022-12-31 14:51:05 +0530

    Well arranged 💍

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ftt/widgets/will_pop_scope.dart';
+import 'package:ftt/widgets/examples.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,7 +15,85 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Flutter Kozhikode'),
       ),
-      body: const WillPopScopeExample(),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              ExampleButton(
+                exampleWidget: PageViewExample(),
+                title: "Page View Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: ExpansionTileExample(),
+                title: "Expansion Tile Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: HeroPageOne(),
+                title: "Hero Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: StepperExample(),
+                title: "Stepper Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: VisibilityExample(),
+                title: "Visibility Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: WillPopScopeExample(),
+                title: "Will Pop Scope Example",
+              ),
+              SizedBox(height: 18),
+              ExampleButton(
+                exampleWidget: ExpandedExample(),
+                title: "Expanded Example",
+              ),
+              SizedBox(height: 18),
+              Divider(
+                color: Colors.blueGrey,
+                thickness: 2,
+                height: 40,
+              ),
+              FlutterLogo(size: 100),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExampleButton extends StatelessWidget {
+  final Widget exampleWidget;
+  final String title;
+  const ExampleButton({
+    super.key,
+    required this.exampleWidget,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 55,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => exampleWidget,
+            ),
+          );
+        },
+        child: Text(title),
+      ),
     );
   }
 }
---
+++ lib/widgets/examples.dart
@@ -0,0 +1,8 @@
+export 'choice_chips.dart';
+export 'expanded.dart';
+export 'expansion_tile.dart';
+export 'hero.dart';
+export 'page_view.dart';
+export 'stepper.dart';
+export 'visibility.dart';
+export 'will_pop_scope.dart';
